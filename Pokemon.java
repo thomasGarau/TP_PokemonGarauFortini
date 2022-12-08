766960f925701de65f14a7f4082f773bc6d86605
Donat Fortini <donatfortini2a@gmail.com>
2022-12-08 11:35:02 +0100

    competence

--- Pokemon.java
+++ Pokemon.java
@@ -9,9 +9,7 @@ public class Pokemon {
     private final int EVO_MAX;
     private ArrayList <String> next_evo=new ArrayList<>(2);
     private ArrayList <String> changement=new ArrayList<>(2);
-    private ArrayList <Competence> Competences;
     
-
     public Pokemon(String nom,ArrayList<String> type,int EVO_MAX,ArrayList<String> next_evo){
         Random r =new Random();
         this.EVO_MAX=EVO_MAX;
@@ -63,7 +61,78 @@ public class Pokemon {
     }
 
     public ArrayList<Competence> getCompetences() {
-        return Competences;
+        return competence;
+    }
+
+    public void assignComp(String type){
+        Competence charge=new Competence("charge", "normal");
+        Competence flameche=new Competence("flameche", "feu");
+        Competence bulles_do=new Competence("bulles'do", "eau");
+        Competence tranch_herbe=new Competence("tranch'herbe", "plante");
+        Competence eclair=new Competence("éclair", "electrik");
+        Competence poing_glace=new Competence("poing glace", "glace");
+        Competence balayette=new Competence("balayette", "combat");
+        Competence dard_venin=new Competence("dard venin", "poison");
+        Competence tunnel=new Competence("tunnel", "sol");
+        Competence picpic=new Competence("picpic", "vol");
+        Competence psyko=new Competence("psyko", "psy");
+        Competence dard_nue=new Competence("dard nuée", "insecte");
+        Competence eboulement=new Competence("éboulement", "roche");
+        Competence griffe_ombre=new Competence("griffe ombre", "spectre");
+        Competence draco_griffe=new Competence("draco-griffe", "dragon");
+        Competence machouille=new Competence("machouille", "ténèbres");
+        Competence gyroballe=new Competence("gyroballe", "acier");
+        if(type=="normal"){
+            competence.add(charge);
+        }
+        else if(type=="feu"){
+            return 1;
+        }
+        else if(type=="eau"){
+            return 2;
+        }
+        else if(type=="plante"){
+            return 3;
+        }
+        else if(type=="électrick"){
+            return 4;
+        }
+        else if(type=="glace"){
+            return 5;
+        }
+        else if(type=="combat"){
+            return 6;
+        }
+        else if(type=="poison"){
+            return 7;
+        }
+        else if(type=="sol"){
+            return 8;
+        }
+        else if(type=="vol"){
+            return 9;
+        }
+        else if(type=="psy"){
+            return 10;
+        }
+        else if(type=="insecte"){
+            return 11;
+        }
+        else if(type=="roche"){
+            return 12;
+        }
+        else if(type=="spectre"){
+            return 13;
+        }
+        else if(type=="dragon"){
+            return 14;
+        }
+        else if(type=="ténèbres"){
+            return 15;
+        }
+        else {
+            return 16;
+        }    
     }
 
     public void evoluer(){
