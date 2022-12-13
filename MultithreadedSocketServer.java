d88ad7f031e1dc1d11fe35769756b36173728f1f
Donat Fortini <donatfortini2a@gmail.com>
2022-12-13 09:58:00 +0100

    sw

--- MultithreadedSocketServer.java
+++ MultithreadedSocketServer.java
@@ -10,7 +10,7 @@ public class MultithreadedSocketServer {
     public static void main(String[] args) throws Exception {
         try{
             ServerSocket server=new ServerSocket(8888);
-            ArrayList <ServerClientThread> listCo=new ArrayList<>();
+            ArrayList <ServerClientThread> listeCo=new ArrayList<>();
             int counter=0;
             System.out.println("Server Started ....");
             while(true){
@@ -18,7 +18,7 @@ public class MultithreadedSocketServer {
                 Socket serverClient=server.accept();  //server accept the client connection request
                 System.out.println(" >> " + "Client No:" + counter + " started!");
                 ServerClientThread sct = new ServerClientThread(serverClient,counter); //send  the request to a separate thread
-                listCo.add(sct);
+                listeCo.add(sct);
                 sct.start();
             }
         }catch(Exception e){
